e52f94e7376ffa07d0a7da060e40accd940cabed
sypopo <sypopo@qq.com>
2019-12-13 14:37:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #change the default theme to argon
 sed -i '14s/bootstrap/argon/' feeds/luci/collections/luci/Makefile
