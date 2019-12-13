5bd88a578847e4f3a8d809934e8617e0a4617485
sypopo <sypopo@qq.com>
2019-12-13 16:48:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,4 +10,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #change the default theme to argon
-sed -i '14s/bootstrap/argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
