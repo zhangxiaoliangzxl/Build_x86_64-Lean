29f4e1b7de74d52d7391ef19483785086755e072
sypopo <sypopo@qq.com>
2019-12-15 14:24:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/Kinodi/luci-theme-atmaterial.git package/lean/atmaterial
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
