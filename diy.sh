28d6d17d21890afa5fd9d6b507af6b6ebc36a5a5
sypopo <sypopo@qq.com>
2019-12-21 21:19:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,8 +7,12 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 删除原主题包
 rm -rf package/lean/luci-theme-argon
+# 添加新的包
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/Kinodi/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# 删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/lean/default-settings/files/zzz-default-settings
