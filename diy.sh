406581b3d287f2babebb566b03f6b82fcd287009
sypopo <sypopo@qq.com>
2019-12-22 09:17:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,8 +11,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 rm -rf package/lean/luci-theme-argon
 # 添加新的包
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/Kinodi/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
-git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
+git https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+# git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
 # 取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 # 删除默认密码
